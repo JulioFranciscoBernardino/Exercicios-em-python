318dd5c2d90b4ac4ec282132bb0103e9e6356102
Julio Francisco Bernardino <juliobernardinoprogramador@gmail.com>
2025-02-07 10:53:51 -0300

    Update 29.py

--- 29.py
+++ 29.py
@@ -6,7 +6,19 @@
 # - 10 anos ou mais: aumento de 20%
 
 def main():
-    pass
+    nome = input("Digite o nome do funcionario: ")
+    salario = float(input("Digite o salário do funcionario: "))
+    anos = int(input("Digite quanto anos o funcionario está na empresa: "))
+
+    if anos < 3:
+        salario = salario + (salario * 0.03)
+        print(f"O funcionario {nome} recebera {salario} que são 3% de aumento")
+    elif anos == 3 and anos <= 10:
+        salario = salario + (salario * 0.125)
+        print(f"O funcionario {nome} recebera {salario} que são 12.5% de aumento")
+    else:
+        salario = salario + (salario * 0.2)
+        print(f"O funcionario {nome} recebera {salario} que são 20% de aumento")
 
 
 if __name__ == "__main__":
